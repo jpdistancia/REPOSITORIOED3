0f43c7c97cbcc67161d7d2cd5bf6c2a4b08f1ba2
Isabel-Puertas9 <i.puertas.planes@gmail.com>
2024-02-27 12:41:02 +0100

    Subir los archivos a GitHub

---
+++ ProyectoED3/src/main/java/com/mycompany/proyectoed3/A.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoed3;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Profesor
+ */
+public class A 
+{
+    public int at1;
+
+    public A(int at1) 
+    {
+        this.at1 = at1;
+    }
+
+    public int getAt1() 
+    {
+        return at1;
+    }
+
+    public void setAt1(int at1) 
+    {
+        this.at1 = at1;
+    }
+    
+    
+   
+    
+    
+    
+    
+     
+    
+}
